1589afa9acf38d6e3c64fa7e968f6082d45df679
yassine <mugiwarajr0@gmail.com>
2023-09-09 17:04:59 +0100

    git

--- src/component/cart/CartEmpty.jsx
+++ src/component/cart/CartEmpty.jsx
@@ -1,10 +1,20 @@
+import { useDispatch } from "react-redux";
+import { setOpernCart } from "../../app/CartSlice";
 import emptybag from "../../assets/emptybag.png";
 
 function CartEmpty() {
+  const dispatch = useDispatch();
+  const onCartToggle = () => {
+    dispatch(
+      setOpernCart({
+        cartState: false,
+      })
+    );
+  };
   return (
     <div className="cartempty">
       <img src={emptybag} alt="" />
-      <button className="cartempty__btn">
+      <button className="cartempty__btn" onClick={onCartToggle}>
         <i className="fa-solid fa-arrow-left"></i>
         <span>Back To Nike Store</span>
       </button>
